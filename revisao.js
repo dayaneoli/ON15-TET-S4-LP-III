a3ae2b4f3cb1d9e15bcbc2928d6426fcbc93070d
dayaneoli <eng.dayaneoliveira@gmail.com>
2022-04-17 11:09:40 -0300

    iiii

---
+++ revisao.js
@@ -0,0 +1,14 @@
+const professora = new Object()
+professora.nome = "Lilit"
+professora.sobrenome = "Bandeira"
+professora.cidade = "São Paulo"
+
+const aluna = {
+nome: { primeiro: "Gabriela", segundo: "Lemos" },
+idade: 20,
+cidade: "São Paulo",
+interesses: ["música", "animes"],
+saudacao: () => console.log("olá meninas"),
+
+};
+console.log(professora)
